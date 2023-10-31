89835b1534d1702ee8072f4ed25b0274e4001e06
euriceliaspaz <euriceliaspaz@gmail.com>
2023-10-31 15:21:41 -0300

    MODIFICADO HISTORIA E IMAGENS

--- lib/dados/atracoes.dart
+++ lib/dados/atracoes.dart
@@ -3,29 +3,31 @@ import 'package:visite_cm/modelos/atracao.dart';
 final atracoes = [
   Atracao(
     id: "tur1",
-    nome: "Açude Grade",
+    nome: "Açude Grande",
     fotos: [
       "https://www.campomaior.pi.gov.br/kcfinder/images/394906_525751784113036_1879819764_n.jpg",
       "https://i.ibb.co/T1bgMpK/image.png",
       "https://www.diariodecampomaior.com.br/images/noticias/114/WhatsApp-Image-2022-03-29-at-13.32.24.jpeg",
+      "https://upload.wikimedia.org/wikipedia/commons/7/7b/A%C3%A7ude_Grande_de_Campo_Maior_02.jpg" ,
     ],
     descricao: [
-      "O Açude Grande de Campo Maior foi construido entre 1858 e 1859, em obra realida pelo Cel. Jacob Almendra.",
-      "Durante anos foi foco de atenção da cidade de Campo Maior, tendo sido as suas margens instalada a conhecida 'Praça da Radar', posteriormente reformada para o Complexo Cultural Valdir Fortes e o restaurante Hawaii.",
-      "O Açude também é o local onde o campomaiorense se exercita, caminhando ao redor de sua orla.",
+      " Campo Maior tem como paisagem característica e cultural o seu açude Grande, que fica às margens da BR 343, sendo também a única reserva de água no perímetro urbano, que serve a população para recreio social, esportivo e contemplativo da natureza e turístico.",
     ],
     localizacao:
         "https://www.google.com/maps/@-4.833189583616752,-42.168444502722956?entry=ttu",
   ),
   Atracao(
     id: "tur2",
-    nome: "Pé da Serra",
+    nome: "Serra de Santo Antonio",
     fotos: [
       "https://causosassustadoresdopiaui.files.wordpress.com/2017/06/17626403_1849107248683605_8142188402617499941_n.jpg",
+      "https://th.bing.com/th/id/OIP.JyRTfeHSGjx9Ew72lNAKPwHaE7?pid=ImgDet&rs=1",
     ],
     descricao: [
-      "O Pé da Serra de Campo Maior é um conjunto de formações rochosas que abriga morros, piscinas naturais, rios e cachoeiras. Oficialmente é conhecido como Serra de Santo Antônio.",
-      "Em um de seus morros foi instaldo um mirante e um crucifixo, acessível por uma escadaria que serve como 'trilha'.",
+      "Criado em 2019, o Parque Estadual da Serra de Santo Antônio fica às margens da PI-215. O lugar reúne trilhas, cachoeiras, mirante e até fazendas que guardam a história do estado. Na subida da serra, o visitante tem contato com uma biodiversidade da flora e fauna, e no fim uma passagem incrível.",
+      "A Serra de Santo Antônio é formada por rochas sedimentares e compõe a Serra da Ibiapaba, que possui uma extensão de 4 mil hectares e faz parte de uma área de preservação ambiental que interliga as cidades de Altos, Campo Maior e Coivaras.",
+
+      
     ],
     localizacao:
         "https://www.google.com/maps/@-5.083204960330369,-42.08333329999999?entry=ttu",
@@ -37,8 +39,8 @@ final atracoes = [
       "https://www.conhecaopiaui.com/images/posts/20190316160042_6hwhf5Sd_995184149435_ujUGsZB9.jpeg",
     ],
     descricao: [
-      "O monumento que marca o local do combate contra as tropas portuguesas, única sangrenta no processo de independência do Brasil.",
-      "Junto ao monumento existe também o Cemitério do Batalhão e o Museu do Couro.",
+      "Berço dos Heróis do Jenipapo Campo Maior foi palco do único confronto sangrento pela Independência do Brasil. Em 13 de março de 1823, às margens do rio Janipapo, camponeses, vaqueiros e sertanejos lutaram contra a resistente tropa comandada pelo português João José da Cunha Fídié.",
+      " A batalha iniciou após terem sido descobertas as intenções do comandante em manter a região sobre o domínio português. Após várias horas de combate, os nordestinos foram derrotados. O ato de bravura fez com que a tropa desviasse seu destino e fosse derrotada no Maranhão. O embate foi crucial para o processo de emancipação politica do Brasil e ficou marcado como a Batalha do Jenipapo",
     ],
     localizacao:
         "https://www.google.com/maps/place/Monumento+aos+Her%C3%B3is+do+Jenipapo/@-4.7823835,-42.1184066,16z",
@@ -62,10 +64,26 @@ final atracoes = [
       "https://piauihoje.com/uploads/editor/1645297661536_51af67432abb48aa152270221948b0ce816508f8.jpg",
     ],
     descricao: [
-      "As barragens Emparedado e Corredores se localizam no Povoado Cacimbas em Campo Maior.",
-      "Ambas as barragens são conhecidas por atrair muitos visitantes durante o período chuvoso, já que fornecem opções acessíveis de banhos.",
+      "Há dez anos, mais precisamente no dia 02 de agosto de 2009, foi inaugurada a obra do Balneário Corredores, que fica localizado na zona rural de Campo Maior (a 82km de Teresina), onde foi realizado um investimento de aproximadamente 600 mil reais pelo Governo do Estado na estrutura que incluiu construção de churrascarias, campo de futebol, playground e banheiros. Com isso aumentou consideravelmente a visita de turistas ao local.",
     ],
     localizacao:
         "https://www.google.com/maps/place/BARRAGEM+DOS+CORREDORES/@-5.1071945,-42.0378374,15z",
+  ),
+   Atracao(
+    id: "tur6",
+    nome: "Catedral Santo Antonio ",
+    fotos: [
+      "https://th.bing.com/th/id/R.389f68f515e13590c9019c0b79c7b7bc?rik=%2fbC7%2ftT8dT3fnA&riu=http%3a%2f%2fdiocesedecampomaior.org.br%2fwp-content%2fuploads%2f2016%2f07%2fcatedral-santo-antonio.jpg&ehk=y74qMIgcF1kvMQdLeIgDTdWwfQUZlmXMQhZVTmdE9dE%3d&risl=&pid=ImgRaw&r=0",
+      "https://1.bp.blogspot.com/-lpGPMgTNlVk/VoHxxm_Y80I/AAAAAAABJ4I/EpBJ5gJn4Cc/s1600/2.JPG",
+      "https://images.app.goo.gl/SeT5Y4bfMNzWrr456",
+    ],
+
+    descricao: [
+      "A Catedral de Santo Antônio é um templo católico situado na Praça Bona Primo, em Campo Maior, no estado do Piauí. Desde 12 de junho de 1976, é Sé episcopal[1] da Diocese de Campo Maior.",
+      "Foi erguida sob o comando do pároco Mateus Rufino sobre os escombros da antiga igrejinha de Santo Antônio cuja construção teve início em 1944 e foi concluída em 1962, em evento comemorativo do bicentenário de Campo Maior. É um monumento da Diocese do mesmo município.",
+    ],
+    localizacao:
+        "https://www.google.com.br/maps/place/Catedral+de+Santo+Ant%C3%B4nio/@-4.823815,-42.1709668,17z/data=!3m1!4b1!4m6!3m5!1s0x7918c08f52f2249:0xb25d77a30deaf74b!8m2!3d-4.823815!4d-42.1683919!16s%2Fg%2F11b6y0jfj5?entry=ttu",
   )
 ];
+ 
